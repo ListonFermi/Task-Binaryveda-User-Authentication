8175b9888e5efb6f1073fdcc2db11b2adce5996a
Liston Fermi <listonfermi@gmail.com>
2024-10-12 17:46:32 +0530

    changed comments

--- backend/src/interface/services/userService.interface.ts
+++ backend/src/interface/services/userService.interface.ts
@@ -1,11 +1,6 @@
 import { UserSignupInput, UserSignupOutput } from "./userService.types";
 
 export interface IUserService {
-  /**
-   * Sign up a new user.
-   * @param userData - The data required for user signup.
-   * @returns A promise that resolves to the user data upon successful signup.
-   */
   userSignup(userData: UserSignupInput): Promise<UserSignupOutput>;
 
   userLogin(email: string, password: string): Promise<UserSignupOutput>;
